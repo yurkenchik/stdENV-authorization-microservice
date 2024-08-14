5d771cd650d4148d27efd13eb79296531a8ec41f
yurkenchik <yura.ilchshyn@gmail.com>
2024-08-15 01:53:07 +0300

    updated

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,11 +1,16 @@
 import {Module} from "@nestjs/common";
+import process from "node:process";
 import {JwtModule} from "@nestjs/jwt";
+import * as dotenv from "dotenv";
 import {TokenService} from "./token.service";
+dotenv.config();
 
 @Module({
     providers: [TokenService],
     imports: [
-        JwtModule,
+        JwtModule.register({
+            secret: "secret",
+        }),
     ],
     exports: [JwtModule, TokenService]
 })
